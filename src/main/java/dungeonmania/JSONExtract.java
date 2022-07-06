e348ab395dbbf15526bce820945f7bcf0ed1ba1d
Shreyas Ananthula <s.ananthula@student.unsw.edu.au>
2022-07-06 14:07:16 +1000

    Update src/main/java/dungeonmania/JSONExtract.java

--- src/main/java/dungeonmania/JSONExtract.java
+++ src/main/java/dungeonmania/JSONExtract.java
@@ -9,19 +9,4 @@ import dungeonmania.Entities.Entity;
 public class JSONExtract {
     
 
-    public static void setConfig(String config) {
-
-    }
-
-    public static List<Entity> extractMapEntites(String Dungeon) {
-        
-        List<Entity> entities = new ArrayList<>();
-
-        
-
-
-        return entities;
-
-    }
-
 }
