ee4b9a0ed3fcb6b4a4fa6e9d0b044239022fed7c
Shreyas Ananthula <s.ananthula@student.unsw.edu.au>
2022-07-06 14:08:03 +1000

    Update src/main/java/dungeonmania/GameController.java

--- src/main/java/dungeonmania/GameController.java
+++ src/main/java/dungeonmania/GameController.java
@@ -7,20 +7,6 @@ import dungeonmania.Entities.*;
 
 public class GameController {
     
-    public List<Entity> game_entities; 
-
-
-    public void newGame(String dungeonName, String config) {
-    
-        JSONExtract.setConfig(config);
-
-        this.game_entities = JSONExtract.extractMapEntites(dungeonName);
-
-        
-
-
-
-    }
 
 
 
