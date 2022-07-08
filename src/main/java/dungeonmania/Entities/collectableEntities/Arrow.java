eb1b5e7e6d017d34b19275ab6382f039df8ac1c6
Shreyas Ananthula <z5360586@ad.unsw.edu.au>
2022-07-08 17:13:55 +1000

    save

--- src/main/java/dungeonmania/Entities/collectableEntities/Arrow.java
+++ src/main/java/dungeonmania/Entities/collectableEntities/Arrow.java
@@ -1,5 +1,7 @@
 package dungeonmania.Entities.collectableEntities;
 
 public class Arrow extends Collectable{
-    
+    public Arrow(String Id, Position position) {
+        super(Id, "arrow", position);
+    }
 }
--- src/main/java/dungeonmania/Entities/collectableEntities/Sword.java
+++ src/main/java/dungeonmania/Entities/collectableEntities/Sword.java
@@ -1,5 +1,9 @@
 package dungeonmania.Entities.collectableEntities;
 
+import dungeonmania.util.Position;
+
 public class Sword extends Collectable{
-    
+    public Sword(String Id, Position position) {
+        super(Id, "sword", position);
+    }
 }
