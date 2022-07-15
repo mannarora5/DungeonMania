175e00ed7a15997815507d11c3f50191292d91dd
Shreyas Ananthula <z5360586@ad.unsw.edu.au>
2022-07-15 19:59:06 +1000

    save

--- src/main/java/dungeonmania/Entities/buildableEntities/Bow.java
+++ src/main/java/dungeonmania/Entities/buildableEntities/Bow.java
@@ -1,5 +1,19 @@
 package dungeonmania.Entities.buildableEntities;
 
+
+import dungeonmania.util.Position;
+
 public class Bow extends Buildable{
+
+
+    public static Integer bowDuration;
+
+    public Bow(String Id, Position position) {
+        super(Id, "bow", position, false);
+    }
+    
+    public static void setbowDuration(Integer duration){
+        Bow.bowDuration = duration;
+    }  
     
 }
