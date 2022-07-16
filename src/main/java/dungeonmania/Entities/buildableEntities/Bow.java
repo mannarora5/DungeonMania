b42430af084d6ca038b5739d9100815e04bef4e9
Shreyas Ananthula <z5360586@ad.unsw.edu.au>
2022-07-16 15:09:03 +1000

    syntax errors

--- src/main/java/dungeonmania/Entities/buildableEntities/Bow.java
+++ src/main/java/dungeonmania/Entities/buildableEntities/Bow.java
@@ -8,12 +8,25 @@ public class Bow extends Buildable{
 
     public static Integer bowDuration;
 
+    public Integer currentBowDuration;
+
     public Bow(String Id, Position position) {
         super(Id, "bow", position, false);
+        this.currentBowDuration = Bow.bowDuration;
     }
     
     public static void setbowDuration(Integer duration){
         Bow.bowDuration = duration;
     }  
+
+
+    public Integer getCurrentBowDuration() {
+        return this.currentBowDuration;
+    }
+
+    public void setCurrentBowDuration(Integer currentBowDuration) {
+        this.currentBowDuration = currentBowDuration;
+    }
+
     
 }
--- src/main/java/dungeonmania/Entities/buildableEntities/Shield.java
+++ src/main/java/dungeonmania/Entities/buildableEntities/Shield.java
@@ -7,8 +7,11 @@ public class Shield extends Buildable{
     public static Integer shieldDuration;
     public static Integer shieldDefence;
 
+    public Integer currentShieldDuration;
+
     public Shield(String Id, Position position) {
         super(Id, "shield", position, false);
+        this.currentShieldDuration = Shield.shieldDuration;
     }
     
     public static void setShielduration(Integer duration){
@@ -18,5 +21,16 @@ public class Shield extends Buildable{
     public static void setSheildDefence(Integer defence){
         Shield.shieldDefence = defence;
     }  
+
+
+    public Integer getCurrentShieldDuration() {
+        return this.currentShieldDuration;
+    }
+
+    public void setCurrentShieldDuration(Integer currentShieldDuration) {
+        this.currentShieldDuration = currentShieldDuration;
+    }
+
+    
     
 }
