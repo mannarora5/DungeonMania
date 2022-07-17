c9fc7c688cce2029cfb1955ed0d0bfed462a738e
Mann Arora <z5360526@ad.unsw.edu.au>
2022-07-17 16:45:42 +1000

    Adjusted code for player & inevntory

--- src/main/java/dungeonmania/Entities/Player/Inventory.java
+++ src/main/java/dungeonmania/Entities/Player/Inventory.java
@@ -203,7 +203,7 @@ public class Inventory {
     }
 
     /**
-     * Check has hey
+     * Check has key
      * @return
      */
     public boolean hasKey(){
@@ -219,17 +219,18 @@ public class Inventory {
     }
 
 
-
-    // Getters and Setters//
-
-
+    /**
+     * Get items
+     */
     public List<Collectable> getItems() {
         return this.items;
     }
 
+    /**
+     * Update items
+     * @param items
+     */
     public void setItems(List<Collectable> items) {
         this.items = items;
     }
-
-    
 }
