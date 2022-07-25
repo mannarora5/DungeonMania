cdf8108a89db5c2a6f4ffd6b20096fac72b30664
Shreyas Ananthula <z5360586@ad.unsw.edu.au>
2022-07-25 13:18:58 +0530

    s

--- src/main/java/dungeonmania/JSONExtract.java
+++ src/main/java/dungeonmania/JSONExtract.java
@@ -208,22 +208,15 @@ public class JSONExtract {
 
 
 
-        // Milestone 3 configs
+        Integer mind_control_duration  = (Integer) configs.get("mind_control_duration");
+        Sceptre.setMindControlDuration(mind_control_duration);
 
-        if (configs.has("mind_control_duration")) {
-            Integer mind_control_duration  = (Integer) configs.get("mind_control_duration");
-            Sceptre.setMindControlDuration(mind_control_duration);
-        }
+        Integer midnight_armour_attack  = (Integer) configs.get("midnight_armour_attack");
+        MidnightArmour.setArmourAttack(midnight_armour_attack);
+    
+        Integer midnight_armour_defence  = (Integer) configs.get("midnight_armour_defence");
+        MidnightArmour.setArmourDefence(midnight_armour_defence);
 
-        if (configs.has("midnight_armour_attack")){
-            Integer midnight_armour_attack  = (Integer) configs.get("midnight_armour_attack");
-            MidnightArmour.setArmourAttack(midnight_armour_attack);
-        }
-
-        if (configs.has("midnight_armour_defence")) {
-            Integer midnight_armour_defence  = (Integer) configs.get("midnight_armour_defence");
-            MidnightArmour.setArmourDefence(midnight_armour_defence);
-        }
 
 
     }
